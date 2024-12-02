297d4734d11f3dd3d6d4fb51797e9ed9a8772bd9
KOrtizLedezma <kenethortiz2016@gmail.com>
2024-12-02 14:23:33 -0500

    Main changes merged, all features finished, readdy to deplooy

--- geoquiz/src/Components/Dashboard/RecentActivityComponent.jsx
+++ geoquiz/src/Components/Dashboard/RecentActivityComponent.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { useScores, useState } from '@/Contexts/ProfileContext';
-import "../../Styles/DashboardStyles/TabsStyles.css";
+import { useScores } from '@/Contexts/ProfileContext';
+import "../../Styles/DashboardStyles/DashboardPageStyles.css";
 
 function RecentActivityComponent() {
 
@@ -14,21 +14,23 @@ function RecentActivityComponent() {
         <div className="recent-activity">
             <h2 className="recent-activity-title">Recent Activity</h2>
             {validActivities.length > 0 ? (
-                <ul className="activity-list">
-                    {validActivities.map((activity, index) => (
-                        <li key={index} className="activity-item">
-                            <p>
-                                <span className="activity-label">Score:</span> {activity.score}
-                            </p>
-                            <p>
-                                <span className="activity-label">Difficulty:</span> {activity.difficulty}
-                            </p>
-                            <p>
-                                <span className="activity-label">Date:</span> {new Date(activity.date).toLocaleString()}
-                            </p>
-                        </li>
-                    ))}
-                </ul>
+                <div className="activity-list-container">
+                    <ul className="activity-list">
+                        {validActivities.map((activity, index) => (
+                            <li key={index} className="activity-item">
+                                <p>
+                                    <span className="activity-label">Score:</span> {activity.score}
+                                </p>
+                                <p>
+                                    <span className="activity-label">Difficulty:</span> {activity.difficulty}
+                                </p>
+                                <p>
+                                    <span className="activity-label">Date:</span> {new Date(activity.date).toLocaleString()}
+                                </p>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
             ) : (
                 <p className="no-activity">No recent activity to display.</p>
             )}
